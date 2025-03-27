a6ed57de3d1580a82caf61a74a9c3f477cc07d54
Benny-Zheng <yzhen175@asu.edu>
2025-03-26 19:32:25 -0700

    Update HW3TestMain.java

--- HW3/FoundationCode/FoundationCode/src/HW3/HW3TestMain.java
+++ HW3/FoundationCode/FoundationCode/src/HW3/HW3TestMain.java
@@ -1,3 +1,5 @@
+package HW3;
+
 /**
  * HW3TestMain.java
  * 
@@ -5,6 +7,7 @@
  * It demonstrates how to use manual test mainlines for checking logic and GUI interaction.
  */
 public class HW3TestMain {
+
     public static void main(String[] args) {
         System.out.println("=================== HW3 Automated Tests ===================");
 
